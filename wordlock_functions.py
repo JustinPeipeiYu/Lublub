365063481640b6528dd24fbd34cff6ace32269b8
Justin Peipei ("Pig") Yu <54873542+JustinPeipeiYu@users.noreply.github.com>
2020-01-26 19:46:04 -0500

    Update wordlock_functions.py

--- wordlock_functions.py
+++ wordlock_functions.py
@@ -84,31 +84,36 @@ def change_state(game_state: str, section_num: int, move: str) -> str:
     Pre-condition: the section number is valid
     >>>change_state("CATDOGFOXEMU",4,'R')
     "CATDOGFOXUEM"
-    >>>change_state("CATDOGFOXUEM",4,'R')
-    "CATDOGFOXMUE"
+    >>>change_state("CATDOGFOXUEM",3,'S')
+    "CATDOGXOFUEM"
     """
     
-    #for this particular function, compute the last sub section
-    last_section = len(game_state) / SECTION_LENGTH    
+    #for this particular function, compute the last sub section by counting all the whole sections
+    last_section = len(game_state) // SECTION_LENGTH   
     
-    #get the starting index
-    start = get_section_start(section_num)
+    #get the first index of sub section
+    first = get_section_start(section_num)
+    
+    
+    #get the first character
+    first_character = game_state[first]
+    
+    #determine whether the sub section is at end or not at end of word, then:
+    if last_section == section_num:
+        #get the last index when the sub section is the last sub section
+        last = len(game_state) - 1
+    else:
+        #get the last index when the sub section is somewhere at middle or beginning
+        last = first + SECTION_LENGTH - 1
+    
+    #get last character
+    last_character = game_state[last]      
+        
+    #store the new game_state
+    new_state = ""     
     
     #if move is swap, then swap the last and first characters of sub section
-    if move == swap:
-        first_character = game_state[start]
-        if last_section >= section_number:
-            #get the last index when the sub section is the last sub section
-            last = len(game_state) - 1
-        else:
-            #get the last index when the sub section is somewhere in the middle of word
-            last = start + SECTION_LENGTH - 1
-        
-        #get last character
-        last_character = game_state[last]      
-        
-        #store the new game_state
-        new_state = ""
+    if move == SWAP:
         
         #build a new game_state switching last character and first characters
         for i in range(len(game_state)):
@@ -118,4 +123,22 @@ def change_state(game_state: str, section_num: int, move: str) -> str:
                 new_state += last_character
             else:
                 new_state += game_state[i]
+    
+    #if move is rotate, then move the last character to the front of subsection, moving
+    #all the other index elements of sub section up one
+    elif move == ROTATE:
+        
+        #build a new game_state moving the every element past start of sub section up one space
+        #until the last index of sub section (go backwards from end to beginning)
+        for i in range(len(game_state)):
+            if i == first:
+                new_state += last_character
+            elif i == first + 1:
+                new_state += first_character
+            elif i>first and i<= last:
+                new_state += game_state[i-1]
+            else:
+                new_state += game_state[i]
+                
+    return new_state
     
